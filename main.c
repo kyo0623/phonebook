6e1bc4eb306aa2973b229fa682897ad12a17f71b
FU JHONG YANG <gomak0829@gmail.com>
2015-10-11 22:19:03 +0800

    Implement hash table method and reduced struct size
    We use hash table method and linked list to accelerate the search time.
    We make new struct to store lastname and modify original struct.
    The hash function is use djb2 algorithm
    
    *Improve search time and reduced cache miss frequency
    *Add new struct and modify old struct

--- main.c
+++ main.c
@@ -8,14 +8,15 @@
 
 #define DICT_FILE "./dictionary/words.txt"
 
+
 static double diff_in_second(struct timespec t1, struct timespec t2)
 {
     struct timespec diff;
     if (t2.tv_nsec-t1.tv_nsec < 0) {
-        diff.tv_sec  = t2.tv_sec  - t1.tv_sec  - 1;
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
         diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
     } else {
-        diff.tv_sec  = t2.tv_sec  - t1.tv_sec;
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec;
         diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
     }
     return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
@@ -37,38 +38,57 @@ int main(int argc, char *argv[])
     }
 
     /* build the entry */
+#ifdef hash_method
+    entry *pHead[hash_bucket_size];
+    entry *e[hash_bucket_size];
+    for(i=0 ; i < hash_bucket_size ; i++) {
+        pHead[i] = (entry *) malloc(sizeof(entry));
+        pHead[i] -> pNext = NULL;
+        e[i] = pHead[i];
+    }
+    i = 0;
+#else
     entry *pHead, *e;
     pHead = (entry *) malloc(sizeof(entry));
-    printf("size of entry : %lu bytes\n", sizeof(entry));
     e = pHead;
     e->pNext = NULL;
+#endif
+    printf("size of entry : %lu bytes\n", sizeof(entry));
 
 #if defined(__GNUC__)
     __builtin___clear_cache((char *) pHead, (char *) pHead + sizeof(entry));
 #endif
+
     clock_gettime(CLOCK_REALTIME, &start);
     while (fgets(line, sizeof(line), fp)) {
         while (line[i] != '\0')
             i++;
-        line[i-1] = '\0';
+        line[i - 1] = '\0';
         i = 0;
+#ifdef hash_method
+        append(line, e);
+#else
         e = append(line, e);
+#endif
     }
     clock_gettime(CLOCK_REALTIME, &end);
     cpu_time1 = diff_in_second(start, end);
 
     /* close file as soon as possible */
     fclose(fp);
-
+#ifdef hash_method
+    for(i=0; i<hash_bucket_size; i++)
+        e[i] = pHead[i];
+#else
     e = pHead;
+#endif
 
     /* the givn last name to find */
     char input[MAX_LAST_NAME_SIZE] = "zyxel";
-    e = pHead;
 
     assert(findName(input, e) &&
            "Did you implement findName() in " IMPL "?");
-    assert(0 == strcmp(findName(input, e)->lastName, "zyxel"));
+    assert(0 == strcmp(findName(input, e) -> lastName, "zyxel"));
 
 #if defined(__GNUC__)
     __builtin___clear_cache((char *) pHead, (char *) pHead + sizeof(entry));
@@ -83,7 +103,11 @@ int main(int argc, char *argv[])
     printf("execution time of findName() : %lf sec\n", cpu_time2);
 
     /* FIXME: release all allocated entries */
+#ifdef hash_method
+    for(i=0; i<hash_bucket_size; i++)
+        free(pHead[i]);
+#else
     free(pHead);
-
+#endif
     return 0;
 }
